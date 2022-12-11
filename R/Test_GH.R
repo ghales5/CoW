93ffef8ef8e5ab1ee289a178ad6ea15c7e4a3be1
Hales <HalesG@wh.org.au>
2022-12-12 08:34:26 +1100

    poa

--- R/Test_GH.R
+++ R/Test_GH.R
@@ -12,51 +12,51 @@ library(tidyverse)
 library(sf)
 
 #=================== LGAs =================================
-wphu_shp <- st_read('../data-raw/WPHU.LGA/WPHU_LGAs.shp')
-wphu_erp <- read.csv('../data-raw/ERP_LGA.csv')
+wphu_shp <- st_read('data-raw/WPHU.LGA/WPHU_LGAs.shp')
+wphu_erp <- read.csv('data-raw/ERP_LGA.csv')
 
-# Get centroids of LGAs, population data and weighted lat/long
-wphu_shp2 <- wphu_shp %>%
-  mutate(centroids = st_centroid(st_geometry(.)),
-         x = unlist(map(centroids,1)),
-         y = unlist(map(centroids,2)))%>%
-  inner_join(wphu_erp, by=c("ABB_NAME" = "LGA")) %>%
-  mutate(Xw = (x*Population),
-         Yw = (y*Population))
-  
-# Calculate the weighted centroid         
-weighted_centre <- wphu_shp2 %>%
-  summarise(sXw = sum(Xw),
-            sYw = sum(Yw),
-            sP = sum(Population),
-            longitude = sXw/sP,
-            latitude = sYw/sP)
+# # Get centroids of LGAs, population data and weighted lat/long
+# wphu_shp2 <- wphu_shp %>%
+#   mutate(centroids = st_centroid(st_geometry(.)),
+#          x = unlist(map(centroids,1)),
+#          y = unlist(map(centroids,2)))%>%
+#   inner_join(wphu_erp, by=c("ABB_NAME" = "LGA")) %>%
+#   mutate(Xw = (x*Population),
+#          Yw = (y*Population))
+#
+# # Calculate the weighted centroid
+# weighted_centre <- wphu_shp2 %>%
+#   summarise(sXw = sum(Xw),
+#             sYw = sum(Yw),
+#             sP = sum(Population),
+#             longitude = sXw/sP,
+#             latitude = sYw/sP)
 
 # Create the WPHU outline and find centroid
 wphu_outline <- wphu_shp %>%
   st_make_valid() %>%
-  st_union() 
+  st_union()
 
 centroid <-  as.data.frame(st_centroid(st_geometry(wphu_outline)))%>%
   mutate(longitude = unlist(map(geometry,1)),
          latitude = unlist(map(geometry,2)))
 
-# Plot together
-wphu_points <- wphu_shp %>%
-  ggplot()+
-  geom_sf() +
-  geom_point(data=centroid, aes(x=longitude, y=latitude, colour = 'centre')) +
-  geom_point(data=weighted_centre, aes(x=longitude, y=latitude, colour = 'weighted centre'), shape = 17) +
-  ggtitle("The centres of WPHU, centre \n and population weight centre")
-  
-wphu_points
+# # Plot together
+# wphu_points <- wphu_shp %>%
+#   ggplot()+
+#   geom_sf() +
+#   geom_point(data=centroid, aes(x=longitude, y=latitude, colour = 'centre')) +
+#   geom_point(data=weighted_centre, aes(x=longitude, y=latitude, colour = 'weighted centre'), shape = 17) +
+#   ggtitle("The centres of WPHU, centre \n and population weight centre")
+#
+# wphu_points
 
 #=================== Meshblocks ================================= More accurate
 
-wphu_MB_erp <- read.csv('../data-raw/MB_Counts.csv') %>%
+wphu_MB_erp <- read.csv('data-raw/MB_Counts.csv') %>%
   mutate(ID= as.character(MB_CODE_2021))
 
-MB_shp <- st_read('../data-raw/MB_2021_WPHU_SHP_GDA94/MB_2021_WPHU_GDA94.shp')
+MB_shp <- st_read('data-raw/MB_2021_WPHU_SHP_GDA94/MB_2021_WPHU_GDA94.shp')
 
 # Get centroids of MBs, population data and weighted lat/long
 wphu_mb_shp <- MB_shp %>%
@@ -67,7 +67,7 @@ wphu_mb_shp <- MB_shp %>%
   mutate(Xw = (x*Person),
          Yw = (y*Person))
 
-# Calculate the weighted centroid         
+# Calculate the weighted centroid
 weighted_centre_MB <- wphu_mb_shp %>%
   summarise(sXw = sum(Xw),
             sYw = sum(Yw),

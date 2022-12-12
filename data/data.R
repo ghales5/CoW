dff90bc354283bfe57a9f7e11a0f764a1fe5f952
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-12-12 12:56:58 +1100

    add docs for outline shape

---
+++ data/data.R
@@ -0,0 +1,11 @@
+#' Shapefile for Western Public Health Unit
+#'
+#' A shapefile containing the outer structure of the Western Public Health Unit.
+#' Created using the mathematical union of the LGAs of interest.
+#'
+#' @format ## `shp_wphu_outline`
+#' Geometry set for 1 feature
+#' Geometry type: POLYGON
+#' Dimension:     XY
+#' Bounding box:  xmin: 144.4441 ymin: -38.00457 xmax: 144.9914 ymax: -37.5524
+#' Geodetic CRS:  GCS_GDA2020

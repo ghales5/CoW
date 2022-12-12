66f2edac1c85aa72d451fa176ed0e0ed11e43b1a
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-12-12 12:57:22 +1100

    Select less columns, convert away from shapefile for fast compute

--- data-raw/wphu_mb_shp.R
+++ data-raw/wphu_mb_shp.R
@@ -19,12 +19,23 @@ wphu_mb_shp <- MB_shp %>%
 usethis::use_data(wphu_mb_shp)
 
 weighted_centre_MB <- wphu_mb_shp %>%
-    summarise(
+    dplyr::select(
+        Xw,
+        Yw,
+        Person
+    ) %>%
+    tibble::as_tibble() %>%
+    dplyr::summarise(
         sXw = sum(Xw),
         sYw = sum(Yw),
         sP = sum(Person),
         centre_longitude = sXw / sP,
         centre_latitude = sYw / sP
+    ) %>%
+    dplyr::select(
+        -sXw,
+        -sYw,
+        -sP
     )
 
 usethis::use_data(weighted_centre_MB)

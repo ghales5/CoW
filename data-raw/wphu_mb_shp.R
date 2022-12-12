3de8b5735eed2998e73712aed2a0e95014525702
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-12-12 12:59:27 +1100

    scope dplyr and sf functions in data-raw

--- data-raw/wphu_mb_shp.R
+++ data-raw/wphu_mb_shp.R
@@ -1,17 +1,17 @@
 wphu_MB_erp <- read.csv("data-raw/MB_Counts.csv") %>%
-    mutate(ID = as.character(MB_CODE_2021))
+    dplyr::mutate(ID = as.character(MB_CODE_2021))
 
-MB_shp <- st_read("data-raw/MB_2021_WPHU_SHP_GDA94/MB_2021_WPHU_GDA94.shp")
+MB_shp <- sf::st_read("data-raw/MB_2021_WPHU_SHP_GDA94/MB_2021_WPHU_GDA94.shp")
 
 # Get centroids of MBs, population data and weighted lat/long
 wphu_mb_shp <- MB_shp %>%
-    mutate(
-        centroids = st_centroid(st_geometry(.)),
+    dplyr::mutate(
+        centroids = sf::st_centroid(st_geometry(.)),
         x = unlist(map(centroids, 1)),
         y = unlist(map(centroids, 2))
     ) %>%
-    inner_join(wphu_MB_erp, by = c("MB_CODE21" = "ID")) %>%
-    mutate(
+    dplyr::inner_join(wphu_MB_erp, by = c("MB_CODE21" = "ID")) %>%
+    dplyr::mutate(
         Xw = (x * Person),
         Yw = (y * Person)
     )

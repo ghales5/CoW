6a8b176183abc1b8869a758c6fcc970c2b47def2
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-12-12 12:57:43 +1100

    Add shape processing file

--- data-raw/wphu_shp.R
+++ data-raw/wphu_shp.R
@@ -1,12 +1,12 @@
 
-wphu_shp <- st_read("data-raw/WPHU.LGA/WPHU_LGAs.shp")
+shp_wphu <- st_read("data-raw/WPHU.LGA/WPHU_LGAs.shp")
 
-usethis::use_data(wphu_shp, overwrite = TRUE)
+usethis::use_data(shp_wphu, overwrite = TRUE)
 
 # Creates shapefiles for the entire WPHU Shapefile
 # without the LGA lines.
-wphu_outline <- wphu_shp %>%
+shp_wphu_outline <- shp_wphu %>%
     st_make_valid() %>%
     st_union()
 
-usethis::use_data(wphu_outline, overwrite = TRUE)
+usethis::use_data(shp_wphu_outline, overwrite = TRUE)

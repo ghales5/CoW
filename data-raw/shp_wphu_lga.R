1dfa114b3ee591884eae112c40f992d200d4ba29
ghales5 <116126735+ghales5@users.noreply.github.com>
2023-01-19 16:19:08 +1100

    usethis LGA changes

--- data-raw/shp_wphu_lga.R
+++ data-raw/shp_wphu_lga.R
@@ -1,4 +1,22 @@
-# TODO: Add population to this shapefile.
-shp_wphu_lga <- st_read("data-raw/WPHU.LGA/WPHU_LGAs.shp")
+## creating WPHU LGA dataset
+# Read lga shapefile
+shp_lga <- st_read("data-raw/WPHU.LGA/WPHU_LGAs.shp")
+
+# Read lga estimate resident population
+erp_wphu_lga <- read.csv("data-raw/ERP_LGA.csv") %>%
+  filter(LPHU == "WPHU")
+
+# Get centroids of LGAs, population data and weighted lat/long
+shp_wphu_mb <- shp_lga %>%
+  dplyr::mutate(centroids = sf::st_centroid(st_geometry(.)),
+    x = unlist(map(centroids, 1)),
+    y = unlist(map(centroids, 2))
+  ) %>%
+  dplyr::inner_join(erp_wphu_lga, by = c("LGA_NAME22" = "LGA")) %>%
+  dplyr::mutate(
+    Xw = (x * Person),
+    Yw = (y * Person)
+  )
+
 
 usethis::use_data(shp_wphu_lga, overwrite = TRUE)

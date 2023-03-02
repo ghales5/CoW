42032d2535029fc394b66bb4689d415645e6379c
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-03-02 13:10:13 +1100

    Add postcode data files

--- data-raw/shp_wphu_lga.R
+++ data-raw/shp_wphu_lga.R
@@ -7,16 +7,16 @@ erp_wphu_lga <- read.csv("data-raw/ERP_LGA.csv") %>%
   filter(LPHU == "WPHU")
 
 # Get centroids of LGAs, population data and weighted lat/long
-shp_wphu_mb <- shp_lga %>%
-  dplyr::mutate(centroids = sf::st_centroid(st_geometry(.)),
+shp_wphu_lga <- shp_lga %>%
+  dplyr::mutate(
+    centroids = sf::st_centroid(st_geometry(.)),
     x = unlist(map(centroids, 1)),
     y = unlist(map(centroids, 2))
   ) %>%
   dplyr::inner_join(erp_wphu_lga, by = c("LGA_NAME22" = "LGA")) %>%
   dplyr::mutate(
-    Xw = (x * Person),
-    Yw = (y * Person)
+    Xw = (x * Population),
+    Yw = (y * Population)
   )
-
 
 usethis::use_data(shp_wphu_lga, overwrite = TRUE)
--- data-raw/shp_wphu_poa.R
+++ data-raw/shp_wphu_poa.R
@@ -3,12 +3,13 @@
 shp_poa <- st_read("data-raw/WPHU.POA/WPHU_Postcodes.shp")
 
 # Read lga estimate resident population
-erp_wphu_poa <- read.csv("data-raw/ERP_POA.csv")%>%
+erp_wphu_poa <- read.csv("data-raw/ERP_POA.csv") %>%
   filter(LPHU == "WPHU")
 
 # Get centroids of Postcodes, population data and weighted lat/long
-shp_wphu_poa <- shp_lga %>%
-  dplyr::mutate(centroids = sf::st_centroid(st_geometry(.)),
+shp_wphu_poa <- shp_poa %>%
+  dplyr::mutate(
+    centroids = sf::st_centroid(st_geometry(.)),
     x = unlist(map(centroids, 1)),
     y = unlist(map(centroids, 2))
   ) %>%

b2debc2068dc3e3def6e0345f75cffa20dd9ed50
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-04-14 10:56:44 +1000

    add trimming function

---
+++ R/trim_to_wphu.R
@@ -0,0 +1,34 @@
+#' Trim points to within WPHU (Western Public Health Unit)
+#'
+#' This function takes a set of points and determines which points fall within
+#' WPHU (Western Public Health Unit) based on their longitude and latitude
+#' coordinates. The function uses the 'sf' package to create points and check
+#' if they intersect with the outline of the WPHU shapefile provided to the
+#' function. If a point is within the WPHU, it is kept in the final output.
+#' If a point is outside of the WPHU, it is discarded from the final output.
+#'
+#' @param pts a data frame of points containing columns 'longitude' and 'latitude'
+#' @return a data frame of points that are within WPHU
+#'
+#' @examples
+#'
+#' data(example_lga_person_weights)
+#'
+#' trim_to_wphu(example_lga_person_weights)
+#'
+#' @importFrom sf sfc_point st_intersects
+#' @importFrom purrr map2 unlist
+#' @export
+trim_to_wphu <- function(pts) {
+    inside_wphu <- purrr::map2(
+        .x = pts$longitude,
+        .y = pts$latitude,
+        .f = function(long, lat) {
+            sfc_point(long, lat) |>
+                st_intersects(shp_wphu_outline) |>
+                length()
+        }
+    ) |> unlist()
+
+    pts[pts[inside_wphu] > 0]
+}

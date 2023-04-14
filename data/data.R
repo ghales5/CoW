6ae0f995d3739fbe34dce4c2408d7a0fcda00e5a
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-04-14 10:58:50 +1000

    Add docs for example person weights

--- data/data.R
+++ data/data.R
@@ -9,3 +9,30 @@
 #' Dimension:     XY
 #' Bounding box:  xmin: 144.4441 ymin: -38.00457 xmax: 144.9914 ymax: -37.5524
 #' Geodetic CRS:  GCS_GDA2020
+
+#' Example LGA weights data using LGA centroids
+#'
+#' A list of two example datasets using LGA centroids with varied person weights.
+#'
+#' @format ## `example_lga_person_weights`
+#' [[1]]
+#'      LGA_NAME22 longitude  latitude person_weight
+#' 1      Brimbank  144.8033 -37.74752            22
+#' 2   Hobsons Bay  144.8298 -37.85471            95
+#' 3   Maribyrnong  144.8778 -37.79525            18
+#' 4     Melbourne  144.9438 -37.81064            98
+#' 5        Melton  144.6260 -37.69296            64
+#' 6 Moonee Valley  144.8958 -37.74948            22
+#' 7      Moreland  144.9489 -37.72990            66
+#' 8       Wyndham  144.6183 -37.88663            27
+#'
+#' [[2]]
+#'      LGA_NAME22 longitude  latitude person_weight
+#' 1      Brimbank  144.8033 -37.74752            52
+#' 2   Hobsons Bay  144.8298 -37.85471            14
+#' 3   Maribyrnong  144.8778 -37.79525            87
+#' 4     Melbourne  144.9438 -37.81064            24
+#' 5        Melton  144.6260 -37.69296            71
+#' 6 Moonee Valley  144.8958 -37.74948            37
+#' 7      Moreland  144.9489 -37.72990            81
+#' 8       Wyndham  144.6183 -37.88663            24

e3be4adc0388f8ea05350ec13daf35748b95765e
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-05-03 11:05:14 +1000

    add optional mapbox token argument to the shiny launcher

--- R/wphu_shiny.R
+++ R/wphu_shiny.R
@@ -1,5 +1,9 @@
 #' @export
-runWPHUCentre <- function(...) {
+runWPHUCentre <- function(mapbox_access_token = NULL, ...) {
+  if (!is.null(mapbox_access_token)) {
+    options(rdeck.mapbox_access_token = mapbox_access_token)
+  }
+
   appDir <- system.file("shiny", "wphu_gis", package = "CoW")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)

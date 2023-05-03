2a33ed140f78254efc65475bbf5c764a678489d9
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-05-03 10:30:10 +1000

    add documentation for check_layer_name

--- R/check_layer_name.R
+++ R/check_layer_name.R
@@ -1,3 +1,14 @@
+#' Check if a layer exists in an rdeck
+#'
+#' This function checks if a specified layer name exists in a deck in the form
+#' of an rdeck object.
+#' The function returns a logical value indicating whether the layer name exists
+#' in the deck.
+#'
+#' @param rdeck an rdeck object
+#' @param layer_name a character string specifying the name of the layer to check
+#' @return a logical value indicating whether the layer exists in the deck
+#'
 #' @export
 check_layer_name <- function(rdeck, layer_name) {
     layers_check <- vapply(rdeck$x$layers, function(layer) {
